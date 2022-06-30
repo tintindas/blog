b84beba3d01d004ceead53a4a170ea5a31a3685f
Upamanyu Das <upamanyu.das.work@gmail.com>
2022-06-30 22:58:51 +0200

    add ProfileImage component

---
+++ src/components/ProfileImage.svelte
@@ -0,0 +1,22 @@
+<div>
+    <div class="image-container">
+        <img src="/profile-800.webp" alt="Upamanyu Das, in a red shirt holding a brown puppy." />
+    </div>
+</div>
+
+<style>
+    .image-container{
+        width: 300px;
+        height: 300px;
+        padding: 2px;
+    }
+    
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        border-radius: 50%;
+        padding: 3px;
+        border: 1px solid var(--secondary)
+    }
+</style>

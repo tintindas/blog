3bd1a9d961e610027c5491ceffd9876c5d99d595
Upamanyu Das <upamanyu.das.work@gmail.com>
2022-07-14 17:01:36 +0200

    fix navbar links and branding

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,35 +1,55 @@
 <script>
 </script>
 
-<!-- TODO: Nav Routing -->
 <!-- TODO: Branding -->
+<!-- TODO: Tool tips -->
 
 <nav>
+	<a href="/"><h1>UD</h1></a>
 	<ul>
-		<li>Home</li>
-		<li>About</li>
-		<li><img src="/github.svg" alt="Github link" /></li>
-		<li><img src="/linkedin.svg" alt="LinkedIn link" /></li>
+		<li><a href="/"><img src="/home.svg" alt="Link to home page of the blog" /></a></li>
+		<li><a href="/about"><img src="/info.svg" alt="Link to about page" /></a></li>
+		<li>
+			<a href="https://github.com/tintindas"><img src="/github.svg" alt="Github link" /> </a>
+		</li>
+		<li>
+			<a href="https://www.linkedin.com/in/upamanyu-das/">
+				<img src="/linkedin.svg" alt="LinkedIn link" />
+			</a>
+		</li>
 	</ul>
 </nav>
 
 <style>
 	nav {
 		width: 100%;
+		display: flex;
+		align-items: center;
+	}
+
+	h1 {
+		margin: 1rem 0;
+		margin-left: 2vw;
+		font-family: 'Parisienne', cursive;
 	}
 
 	ul {
+		margin-left: auto;
 		display: flex;
 		list-style: none;
 		align-items: center;
 		font-size: 1.2rem;
 		justify-content: end;
 		color: hsla(var(--primary));
-		margin-top: 3vh;
 		margin-right: 1vw;
 	}
 
 	li {
 		margin: 1vh 1vw;
+	}
+
+	a {
+		text-decoration: none;
+		color: hsla(var(--primary));
 	}
 </style>

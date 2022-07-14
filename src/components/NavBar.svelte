d1c21ac0c42555c93ebb38e16affe67317aa9707
Upamanyu Das <upamanyu.das.work@gmail.com>
2022-07-14 18:35:28 +0200

    add tooltips

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -4,14 +4,18 @@
 <!-- TODO: Tool tips -->
 
 <nav>
-	<a href="/"><h1>UD</h1></a>
+	<a href="/" class="tooltip branding" data-text="Upamanyu Das"><h1>UD</h1></a>
 	<ul>
-		<li><a href="/"><img src="/home.svg" alt="Link to home page of the blog" /></a></li>
-		<li><a href="/about"><img src="/info.svg" alt="Link to about page" /></a></li>
-		<li>
+		<li class="tooltip nav-link" data-text="Home">
+			<a href="/"><img src="/home.svg" alt="Link to home page of the blog" /></a>
+		</li>
+		<li class="tooltip nav-link" data-text="About">
+			<a href="/about"><img src="/info.svg" alt="Link to about page" /></a>
+		</li>
+		<li class="tooltip nav-link" data-text="Github">
 			<a href="https://github.com/tintindas"><img src="/github.svg" alt="Github link" /> </a>
 		</li>
-		<li>
+		<li class="tooltip nav-link" data-text="LinkedIn">
 			<a href="https://www.linkedin.com/in/upamanyu-das/">
 				<img src="/linkedin.svg" alt="LinkedIn link" />
 			</a>
@@ -50,5 +54,46 @@
 	a {
 		text-decoration: none;
 		color: hsla(var(--primary));
+	}
+
+	.tooltip {
+		position: relative;
+	}
+
+	.tooltip:hover {
+		border-bottom: 2px solid hsla(var(--primary));
+	}
+
+	.tooltip::before {
+		font-size: 0.8rem;
+		color: hsla(var(--secondary) / 0.8);
+		content: attr(data-text);
+		position: absolute;
+
+		bottom: 0;
+		right: 0;
+
+		padding: 2px 5px;
+
+		border-radius: 3px;
+		background: hsla(0 0% 0% / 0.1);
+		display: none;
+	}
+
+	.tooltip:hover::before {
+		display: block;
+	}
+
+	.branding.tooltip::before {
+		right: -50%;
+	}
+
+	.branding.tooltip:hover {
+		border-bottom: unset;
+	}
+
+	.nav-link.tooltip::before {
+		bottom: -100%;
+		right: -50%;
 	}
 </style>
